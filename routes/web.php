c50dd675867244712e6520bc2b529ad0b63e05da
babyboy51 <glosmork@gmail.com>
2020-06-27 22:28:33 +0900

    task13 完了

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create');
     Route::get('news/edit', 'Admin\NewsController@edit');
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
 Auth::routes();
